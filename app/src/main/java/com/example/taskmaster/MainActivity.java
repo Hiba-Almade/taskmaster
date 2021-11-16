97fae3f912399e5a7527a171ca07bad9143712d0
Hiba-Almade <hibaalmade998@gmail.com>
2021-11-16 21:37:00 +0200

    lab 28 done

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,6 +11,9 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,37 +45,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button task1button=findViewById(R.id.labButton);
-        task1button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String task1=task1button.getText().toString();
-                Intent taskDetailsIntent = new Intent(MainActivity.this, taskDetails.class);
-                taskDetailsIntent.putExtra("title",task1);
-                startActivity(taskDetailsIntent);
-            }
-        });
-        Button task2button=findViewById(R.id.cookingButton);
-        task2button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String task2=task2button.getText().toString();
-                Intent taskDetailsIntent = new Intent(MainActivity.this, taskDetails.class);
-                taskDetailsIntent.putExtra("title",task2);
-                startActivity(taskDetailsIntent);
-            }
-        });
-
-        Button task3button=findViewById(R.id.cleanButton);
-        task3button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String task3=task3button.getText().toString();
-                Intent taskDetailsIntent = new Intent(MainActivity.this, taskDetails.class);
-                taskDetailsIntent.putExtra("title",task3);
-                startActivity(taskDetailsIntent);
-            }
-        });
+//        Button task1button=findViewById(R.id.labButton);
+//        task1button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String task1=task1button.getText().toString();
+//                Intent taskDetailsIntent = new Intent(MainActivity.this, taskDetails.class);
+//                taskDetailsIntent.putExtra("title",task1);
+//                startActivity(taskDetailsIntent);
+//            }
+//        });
+//        Button task2button=findViewById(R.id.cookingButton);
+//        task2button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String task2=task2button.getText().toString();
+//                Intent taskDetailsIntent = new Intent(MainActivity.this, taskDetails.class);
+//                taskDetailsIntent.putExtra("title",task2);
+//                startActivity(taskDetailsIntent);
+//            }
+//        });
+//
+//        Button task3button=findViewById(R.id.cleanButton);
+//        task3button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String task3=task3button.getText().toString();
+//                Intent taskDetailsIntent = new Intent(MainActivity.this, taskDetails.class);
+//                taskDetailsIntent.putExtra("title",task3);
+//                startActivity(taskDetailsIntent);
+//            }
+//        });
 
         Button settingbutton=findViewById(R.id.settingButton);
         settingbutton.setOnClickListener(new View.OnClickListener() {
@@ -80,5 +85,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(settingIntent);
             }
         });
+
+        List<Task> tasks=new ArrayList<Task>();
+        tasks.add(new Task("Solve the lab ","solve lab 28","complete"));
+        tasks.add(new Task("cooking","cook rice and meet","assigned"));
+        tasks.add(new Task("clean","clean the home","new"));
+        tasks.add(new Task("shopping","buy something","new"));
+
+
+        RecyclerView recyclerView=findViewById(R.id.recycleTask);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new TaskAdapter(tasks));
+
+
+
     }
 }
